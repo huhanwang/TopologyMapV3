d149d374468e0059ff2d1a006317db829573305a
huahanwang <huhanwang@126.com>
2026-07-30 17:38:10 +0800

    Raise raw visual short line filter

--- algorithms/topology_v3/include/topology_v3/stages/frenet_observation/raw_visual_boundary_preprocessor.h
+++ algorithms/topology_v3/include/topology_v3/stages/frenet_observation/raw_visual_boundary_preprocessor.h
@@ -12,7 +12,7 @@ public:
     struct Config {
         std::string preferred_source = "vehicle_bev_lanes";
         std::size_t min_input_point_count = 2;
-        double min_hard_keep_length_m = 1.0;
+        double min_hard_keep_length_m = 6.0;
         double max_abs_y_vcs_m = 50.0;
         bool keep_passive_semantic_boundaries = true;
     };
